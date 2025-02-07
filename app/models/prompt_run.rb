ae58184f63d510f9746f3db1f38256cd008677f7
billy <billy@hey.com>
2025-02-06 19:52:51 -0800

    Add in prompt run

--- app/models/prompt_run.rb
+++ app/models/prompt_run.rb
@@ -1,4 +1,23 @@
 class PromptRun < ApplicationRecord
   belongs_to :prompt
   belongs_to :run
+
+  has_many_attached :attachments
+
+  after_commit :perform!, on: :create
+
+  def perform!
+    BackgroundRunJob.perform_later(self)
+  end
+
+  def perform
+    result = Stability::ApiWrapper.new.replace_background_and_relight(
+      prompt.subject_image,
+      background_prompt: prompt.background_prompt,
+      background_reference: prompt.background_reference,
+      preserve_original_subject: prompt.preserve_original_subject,
+      original_background_depth: prompt.original_background_depth
+    )
+    attachments.create!(blob: result)
+  end
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -1,22 +1,12 @@
 class Run < ApplicationRecord
   belongs_to :flow
-  has_many_attached :attachments
-  has_one_attached :background_reference
 
-  after_commit :perform!, on: :create
+  after_create :perform!
 
   def perform!
-    BackgroundRunJob.perform_later(self)
-  end
-
-  def perform
-    result = Stability::ApiWrapper.new.replace_background_and_relight(
-      attachments.first,
-      # background_prompt: "commercial food photograph, studio, well lit",
-      background_reference: background_reference,
-      preserve_original_subject: 0.8,
-      original_background_depth: 0.65
-    )
-    attachments.create!(blob: result)
+    
+    flow.prompts.each do |prompt|
+      PromptRun.create!(prompt: prompt, run: self)
+    end
   end
 end
