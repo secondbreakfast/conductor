477fe13630de5b3e8cc64af8d8f70e7ad67a551d
billy <billy@hey.com>
2025-02-07 16:49:21 -0500

    Subject and bg reference

--- app/models/prompt_run.rb
+++ app/models/prompt_run.rb
@@ -12,12 +12,32 @@ class PromptRun < ApplicationRecord
 
   def perform
     result = Stability::ApiWrapper.new.replace_background_and_relight(
-      (run.subject_image || prompt.subject_image),
+      subject_image,
       background_prompt: prompt.background_prompt,
-      background_reference: (run.background_reference || prompt.background_reference),
+      background_reference: background_reference,
       preserve_original_subject: prompt.preserve_original_subject,
       original_background_depth: prompt.original_background_depth
     )
     attachments.create!(blob: result)
+  end
+
+  def subject_image
+    if run.subject_image.attached?
+      run.subject_image
+    elsif prompt.subject_image.attached?
+      prompt.subject_image
+    else
+      nil
+    end
+  end
+
+  def background_reference
+    if run.background_reference.attached?
+      run.background_reference
+    elsif prompt.background_reference.attached?
+      prompt.background_reference
+    else
+      nil
+    end
   end
 end
