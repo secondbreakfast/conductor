87367722ec6b7a806b50db913db34340abb9df94
billy <billy@hey.com>
2025-02-10 18:21:03 -0500

    Updated cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "http://localhost:3010"  # This allows requests from any origin
+    origins "http://localhost:3000",
+            "http://localhost:3010",
+            "https://conductor-production-662c.up.railway.app"
+
     resource "*",
       headers: :any,
       methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
