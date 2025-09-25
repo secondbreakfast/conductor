48972f302674af099d313268404f58827b0b8099
billy <billy@hey.com>
2025-09-25 11:53:19 -0700

    Order

--- app/models/run.rb
+++ app/models/run.rb
@@ -79,7 +79,7 @@ class Run < ApplicationRecord
   end
 
   def data
-    prompt_runs.map(&:data).reduce({}, :merge)
+    prompt_runs.order(:id).map(&:data).reduce({}, :merge)
   end
 
   def output_attachments
