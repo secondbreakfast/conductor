b0350b2899efeada814d707c04aafb458992016a
Ken Miller <ken.miller@owner.com>
2025-11-20 15:03:02 -0800

    Adding hermes to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,6 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
             "http://localhost:3010",
+            "http://localhost:8080",
+            "https://dashboard.owner.com",
             "https://conductor-production-662c.up.railway.app",
             "https://dev-grader.owner.com",
             "https://grader.owner.com"
